a326e5c7742b94921752c0fc99c1b54a1ea85545
GBM1986 <gunnar_bjarni@hotmail.com>
2023-10-24 14:23:13 +0200

    <<aa>>

--- sw.js
+++ sw.js
@@ -1,63 +1,47 @@
-// Var med navn på statsik cache
-const staticCacheName = "stite-static-v1.1";
+// const staticCacheName = 'site-static-v1.2'
 
-const dynamicCacheName = 'site-dynamic-v1.0'
-// Array med filer
 const assets = [
-    '/',
-    '/index.html',
-    '/css/styles.css'
+    "/",
+    "/index.html",
+    "/css/styles.css"
 ]
 
-//Registerer service worker
-
-if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./sw.js')
-	.then(reg => console.log('service worker registered', reg))
-	.catch(err => console.error('service worker not registered', err)) 
-}
-
-// Installerer service worker og cacher nødvendig filer
-
-self.addEventListener ('install', event => {
-    console.log('Service worker has benn installed');
-    //Skriver filer til statisk cache
+// Install Service Worker
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(staticCacheName).then(cache =>{
-            console.log('skriver til statisk cache')
-            cache.addAll(asstets)
+        caches.open(staticCacheName).then(cache => {
+            console.log('write assets files to cache')
+            cache.addAll(assets)
         })
     )
+	console.log('Service Worker has been installed');
 })
 
-// Akitiverer service worker
-
+//Install Service Worker
 self.addEventListener('activate', event => {
-    console.log('Service Worker has been activated');
-    // Skriver tildligere versioner af cache
-    event.waitUntil(
+	console.log('Service Worker has been activated');
+
+    event.waitUntil()
         caches.keys().then(keys => {
             const filteredkeys = keys.filter(key => key !== staticCacheName)
-            filteredkeys.map(key => caches.delete(key))
+            filteredkeys.map(key => {
+                caches.delete(key)
+            })
         })
-    )
 })
 
+
+const dynamicCacheName = 'site-dynamic-v1'
+
 self.addEventListener('fetch', event => {
-    if(!(event.request.url.indexOf('http') === 0)) return
-
-    event.respondwith(
-        caches.match(event.request).then(cacheResult => {
-            return (
-                cacheResult || 
-                fetch(event.request).then(async fetchRes => {
-                    return caches.open(dynamicCacheName).then(cache => {
-                        cache.put(event.request.url, fetchRes.clone())
-
-                        return fetchRes
-                    })
-                })
-            )
-        })
-    )
+	event.respondWith(
+		caches.match(event.request).then(cacheRes => {
+			return cacheRes || fetch(event.request).then(fetchRes => {
+				return caches.open(dynamicCacheName).then(cache => {
+					cache.put(event.request.url, fetchRes.clone())
+					return fetchRes
+				})
+			})
+		})
+	)
 })
