ea4c0ad0e19a41fb5ad7121795a60fe2fe3c2841
GBM1986 <gunnar_bjarni@hotmail.com>
2023-10-24 14:06:21 +0200

    sw2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// const staticCacheName = 'site-static-v1.2'
+const staticCacheName = 'site-static-v1.2'
 
 const assets = [
     "/",
@@ -18,34 +18,27 @@ self.addEventListener('install', event => {
 })
 
 // Install Service Worker
-// self.addEventListener('activate', event => {
-// 	console.log('Service Worker has been activated');
+self.addEventListener('activate', event => {
+	console.log('Service Worker has been activated');
 
-//     event.waitUntil()
-//         caches.keys().then(keys => {
-//             const filteredkeys = keys.filter(key => key !== staticCacheName)
-//             filteredkeys.map(key => {
-//                 caches.delete(key)
-//             })
-//         })
-// })
+    event.waitUntil()
+        caches.keys().then(keys => {
+            const filteredkeys = keys.filter(key => key !== staticCacheName)
+            filteredkeys.map(key => {
+                caches.delete(key)
+            })
+        })
+})
 
 
 const dynamicCacheName = 'site-dynamic-v1'
-// Fetch event
+
 self.addEventListener('fetch', event => {
-	// Kontroller svar på request
 	event.respondWith(
-		// Kig efter file match i cache 
 		caches.match(event.request).then(cacheRes => {
-			// Returner match fra cache - ellers hent fil på server
 			return cacheRes || fetch(event.request).then(fetchRes => {
-				// Tilføjer nye sider til cachen
 				return caches.open(dynamicCacheName).then(cache => {
-					// Bruger put til at tilføje sider til vores cache
-					// Læg mærke til metoden clone
 					cache.put(event.request.url, fetchRes.clone())
-					// Returnerer fetch request
 					return fetchRes
 				})
 			})
