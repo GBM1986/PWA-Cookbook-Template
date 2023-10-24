46d354204966ab6fc588128fab9102e7bc1c2599
GBM1986 <gunnar_bjarni@hotmail.com>
2023-10-24 10:53:11 +0200

    sw

--- sw.js
+++ sw.js
@@ -1,16 +1,28 @@
-if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./sw.js')
-	.then(reg => console.log('service worker registered', reg))
-	.catch(err => console.error('service worker not registered', err)) 
-}
+const staticCacheName = 'site-static-v1.1'
+
+const assets = [
+    "/",
+    "/index.html",
+    "/css/styles.css"
+]
 
 // Install Service Worker
 self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('write assets files to cache')
+            cache.addAll(assets)
+        })
+    )
 	console.log('Service Worker has been installed');
 })
 
 // Install Service Worker
 self.addEventListener('activate', event => {
 	console.log('Service Worker has been activated');
+
+    caches.keys().then(keys => {
+        
+    })
 })
 
