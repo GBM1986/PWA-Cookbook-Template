38e61e31787857a75a682db342e16be1bb5fa51e
GBM1986 <gunnar_bjarni@hotmail.com>
2023-10-24 10:13:59 +0200

    sw

---
+++ sw.js
@@ -0,0 +1,16 @@
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js')
+	.then(reg => console.log('service worker registered', reg))
+	.catch(err => console.error('service worker not registered', err)) 
+}
+
+// Install Service Worker
+self.addEventListener('install', event => {
+	console.log('Service Worker has been installed');
+})
+
+// Install Service Worker
+self.addEventListener('activate', event => {
+	console.log('Service Worker has been activated');
+})
+
