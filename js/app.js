b165bc64826dbcf03f7b8350cb5fa0954568f422
GBM1986 <gunnar_bjarni@hotmail.com>
2023-10-24 14:20:54 +0200

    ddd

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./sw2.js')
+	navigator.serviceWorker.register('./sw.js')
 	.then(reg => console.log('service worker registered', reg))
 	.catch(err => console.error('service worker not registered', err)) 
 }
